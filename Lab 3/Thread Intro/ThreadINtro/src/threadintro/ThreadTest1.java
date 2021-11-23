8211de7c11fa85f0cb5d566e7877a278625b8d82
Ishraq Ahmed <ishraqahmedbd@gmail.com>
2021-11-24 02:15:14 +0600

    update thread intro

---
+++ Lab 3/Thread Intro/ThreadINtro/src/threadintro/ThreadTest1.java
@@ -0,0 +1,14 @@
+package threadintro;
+
+/**
+ *
+ * @author ahmed_ishraq
+ */
+public class ThreadTest1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Current Thread name is: " +Thread.currentThread().getName());
+    }
+
+}

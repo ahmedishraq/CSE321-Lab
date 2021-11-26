ad479d0c522866d0d7b659ad74bb057b0db12122
Ishraq Ahmed <ishraqahmedbd@gmail.com>
2021-11-26 16:09:46 +0600

    initial start of l3 t2

---
+++ Lab 3/Task/src/task/Task2.java
@@ -0,0 +1,36 @@
+package task;
+
+/**
+ *
+ * @author ahmed_ishraq
+ */
+
+class CheckThread extends Thread{
+   // private String name;
+    
+    public CheckThread(String name){
+    super(name);
+    }
+    
+    @Override
+    public void run(){
+        System.out.println("The house is: "+Thread.currentThread().getName());
+    }
+}
+
+
+public class Task2 {
+    public static void main(String [] args){
+        CheckThread hs = new CheckThread("House Stark");
+        CheckThread ht = new CheckThread("House Targaryen");
+        CheckThread hl = new CheckThread("House Lannister");
+        CheckThread hb = new CheckThread("House Bolton");
+        CheckThread htl = new CheckThread("House Tyrell");
+        
+        hs.setPriority(Thread.MAX_PRIORITY);
+        hb.setPriority(Thread.MIN_PRIORITY);
+        
+        hs.start();
+        
+    }
+}

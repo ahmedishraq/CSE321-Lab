b8c2cfa93064bb793f842409da04211aa550590d
Ishraq Ahmed <ishraqahmedbd@gmail.com>
2021-11-26 20:36:31 +0600

    final submit update l3 t3

--- Lab 3/Task/src/task/Task3.java
+++ Lab 3/Task/src/task/Task3.java
@@ -8,8 +8,15 @@ class CustomThread extends Thread {
 
     private Thread t;
     private String check;
-    private int mean = 0;
-    private int sum = 0;
+    private int f_odd_mean = 0;
+    private int f_odd_sum = 0;
+    private int f_even_mean = 0;
+    private int f_even_sum = 0;
+    private int s_odd_mean = 0;
+    private int s_odd_sum = 0;
+    private int s_even_mean = 0;
+    private int s_even_sum = 0;
+    private int avg = 0;
     //private long arr [] = new long[51];
     int arr[] = new int [50];
 
@@ -33,10 +40,9 @@ class CustomThread extends Thread {
         }
     }
 
-    // private int n = 50;
-//    public CustomThread(String check){
-//        this.check = check;
-//    }
+    public CustomThread(String check){
+        this.check = check;
+   }
     @Override
     public void start() {
         if (t == null) {
@@ -46,17 +52,59 @@ class CustomThread extends Thread {
     }
     
     public void first(){
-        for(int i=1;i<50;i=i+2){
-            sum += arr[i];
+        for(int i=1;i<25;i=i+2){
+            f_odd_sum += arr[i];
         }
-        mean = sum/(arr.length/2);
-        System.out.println("mean:"+ mean);
+        f_odd_mean = f_odd_sum/25;
+        //System.out.println("mean:"+ mean);
+    }
+    
+    public void second(){
+        for(int i=0;i<25;i=i+2){
+            f_even_sum += arr[i];
+        }
+        f_even_mean = f_even_sum/25;
+        //System.out.println("mean:"+ mean);
+    }
+    
+    public void third(){
+        for(int i=26;i<50;i=i+2){
+            s_odd_sum += arr[i];
+        }
+        s_odd_mean = s_odd_sum/25;
+        //System.out.println("mean:"+ mean);
+    }
+    
+    public void four(){
+        for(int i=26;i<50;i=i+2){
+            s_even_sum += arr[i];
+        }
+        s_even_mean = s_even_sum/25;
+        //System.out.println("mean:"+ mean);
+    }
+    
+    public void five(){
+        avg = f_odd_mean + f_even_mean + s_odd_mean + s_even_mean;
+        avg = avg/4;
+        System.out.println(avg);
     }
 
     @Override
     public void run() {
         if(check == "first"){
-            
+            first();
+        }
+        else if(check == "second"){
+            second();
+        }
+        else if(check == "third"){
+            third();
+        }
+        else if(check == "four"){
+            four();
+        }
+        else if(check == "five"){
+            five();
         }
     }
 }
@@ -64,9 +112,17 @@ class CustomThread extends Thread {
 public class Task3 {
 
     public static void main(String[] args) {
-        CustomThread c1 = new CustomThread();
-        c1.fib();
-        c1.first();
+        CustomThread c1 = new CustomThread("first");
+        CustomThread c2 = new CustomThread("second");
+        CustomThread c3 = new CustomThread("third");
+        CustomThread c4 = new CustomThread("four");
+        CustomThread c5 = new CustomThread("five");
+        
+        c1.start();
+        c2.start();
+        c3.start();
+        c4.start();
+        c5.start();
 
     }
 }

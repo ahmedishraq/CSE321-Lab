b2a21d21996de6e1cfef7dcfba362feb9c38dee0
Ishraq Ahmed <ishraqahmedbd@gmail.com>
2021-12-02 18:55:21 +0600

    final submit update l4 t1

--- Lab 4/src/Task_1.java
+++ Lab 4/src/Task_1.java
@@ -24,6 +24,7 @@ public class Task_1 {
         int tot_time= 0;
         float avg_waiting_time = 0;
         float avg_turnaround_time = 0;
+        float avg_response_time = 0;
         
         for(int i=0;i<total_process;i++){
             pid[i] = i+1;
@@ -36,7 +37,7 @@ public class Task_1 {
         while(true){
             int min = 999;
             int check = total_process;
-            if(tot_time == 0){
+            if(tot_time == total_process){
                 break;
             }
             for(int i=0;i<total_process;i++){
@@ -57,9 +58,28 @@ public class Task_1 {
                     tot_time++;
                 }
             }
+            // calculation starting time for each process 
+            if( check != total_process){
+                if(rem_brust_time[check] == brust_time[check]-1){
+                     start_time[check] = s_time - 1;
+                }
+            }
         }
-//        for(int i=0;i<total_process;i++){
-//            System.out.println("avgtime: "+waiting_time[i]);
-//        }
+        for(int i=0;i<total_process;i++){
+            turnaround_time[i] = finish_time[i] - arrival_time[i];
+            waiting_time[i] = turnaround_time[i] - brust_time[i];
+            response_time[i] = start_time[i] - arrival_time[i];
+            avg_waiting_time += waiting_time[i];
+            avg_turnaround_time += turnaround_time[i];
+            avg_response_time += response_time[i];
+        }
+        System.out.println("pid  arrival  brust  start  finish  response  turn  waiting");
+        for(int i=0;i<total_process;i++){
+            System.out.println(pid[i]+"\t"+arrival_time[i]+"\t"+brust_time[i]+"\t"+start_time[i]+"\t"+finish_time[i]+"\t"+response_time[i]+"\t"+turnaround_time[i]+"\t"+waiting_time[i]);
+        }
+        System.out.println("");
+        System.out.println("Average Waiting Time: "+(float)avg_waiting_time/total_process);
+        System.out.println("Average Turnaround Time: "+(float)avg_turnaround_time/total_process);
+        System.out.println("Average Response Time: "+(float)avg_response_time/total_process);
     }
 }

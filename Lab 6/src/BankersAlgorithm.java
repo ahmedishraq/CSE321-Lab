dcad10a6efac587a7a4185b6a764d2cd0188cfef
Ishraq Ahmed <ishraqahmedbd@gmail.com>
2021-12-24 20:58:29 +0600

    added txt input file and process no

--- Lab 6/src/BankersAlgorithm.java
+++ Lab 6/src/BankersAlgorithm.java
@@ -17,5 +17,20 @@ public class BankersAlgorithm {
         int [][] available = new int[row+1][column];
         
         LinkedList<Integer> track = new LinkedList<Integer>();
+        
+        String s3 = b.readLine();
+        StringTokenizer st3 = new StringTokenizer(s3," ");
+        int n = 0;
+        while(st3.hasMoreTokens()){
+            process[n] = st3.nextToken();
+            n++;
+        }
+        for(int i=0;i<row;i++){
+            String s = b.readLine();
+            StringTokenizer st = new StringTokenizer(s," ");
+            for(int j=0;j<column;j++){
+                max[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
     }
 }
